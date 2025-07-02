6de5b82839c7a21fa115991276dcd43f7d4c10b8
DNMinhDuc169203 <dangducmtp@gmail.com>
2025-07-02 22:26:50 +0700

    lay thong tin ban than

--- src/Components/ProfileComponets/ReqUserPostPart.jsx
+++ src/Components/ProfileComponets/ReqUserPostPart.jsx
@@ -28,7 +28,7 @@ const ReqUserPostPart = () => {
           const user = JSON.parse(localStorage.getItem("user"));
           if (!user?.id) return;
           const res = await axios.get(
-            `http://localhost:8080/network/api/bai-viet/nguoi-dung/${user.id}`,
+            `http://localhost:8080/network/api/bai-viet/quan-ly`,
             { headers: { Authorization: `Bearer ${token}` } }
           );
           setPosts(res.data.baiViet || []);
